fd8152037aecca9757262d1beef3c4e1f0eafa10
Dave Hanson <drh@drhanson.net>
2007-04-14 15:18:33 +0000

    Add collapsing sections to vita.html.

--- web-content/js/slideshow.js
+++ web-content/js/slideshow.js
@@ -1,5 +1,16 @@
-function slideshow(args) {
+﻿function slideshow(args) {
 	var win = window.open("slideshow.php?" + args, "slideshow",
 		"resizable,height=492,width=600");
 	win.focus();
+}
+
+function details(span, id) {
+	var div = document.getElementById(id);
+	if (span.title == 'Expand') {
+		div.style.display = 'inline';
+		span.title = 'Collapse';
+	} else {
+		div.style.display = 'none';
+		span.title = 'Expand';
+	}
 }
