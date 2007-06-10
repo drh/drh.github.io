d119deae272279e5485f722e47cf4745b5631816
Dave Hanson <drh@drhanson.net>
2007-06-10 02:51:59 +0000

    Fix validation errors, update DTD and encoding.

--- slideshow.php
+++ slideshow.php
@@ -3,19 +3,19 @@ $id = $_GET['id'];
 $doc = $_GET['doc'];
 $title = $_GET['title'] ? $_GET['title'] : "";
 ?>
-<html>
 <head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <title><?= $title ?></title>
-<style>
+<style type="text/css">
 body { margin: 0 0 0 0; padding: 0 0 0 0; }
 object { width: 100%; height: 100%; }
 </style>
 </head>
 <body>
 <object type="application/x-shockwave-flash"
- data="https://s3.amazonaws.com:443/slideshare/ssplayer.swf?id=<?= $id ?>&doc=<?= $doc ?>">
+ data="https://s3.amazonaws.com:443/slideshare/ssplayer.swf?id=<?= $id ?>&amp;doc=<?= $doc ?>">
  <param name="movie"
- value="https://s3.amazonaws.com:443/slideshare/ssplayer.swf?id=<?= $id ?>&doc=<?= $doc ?>" />
+ value="https://s3.amazonaws.com:443/slideshare/ssplayer.swf?id=<?= $id ?>&amp;doc=<?= $doc ?>">
 </object>
 </body>
 </html>
